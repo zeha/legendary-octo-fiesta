dc46c20ece65070f6a7ce2663508c396e7802600
Christian Hofstaedtler <christian.hofstaedtler@deduktiva.com>
2016-10-09 20:43:03 +0200

    you wont believe its not a game

--- code.js
+++ code.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var canvas = document.getElementById('mycanvas');
 var ctx = canvas.getContext('2d');
 
@@ -16,90 +18,136 @@ function rand(min, max) {
     return Math.floor((Math.random() * (max - min + 1)) + min);
 }
 
-// var pendingLoads = assetNames.length;
+var pendingLoads = assetNames.length;
 
-// function showLoadProgress() {
-//     ctx.fillStyle = "yellow";
-//     ctx.font = "14px Helvetica";
-//     ctx.fillText("Loading "+pendingLoads+" of "+assetNames.length+" ...", 200, height/2);
-// }
+function showLoadProgress() {
+    ctx.fillStyle = "yellow";
+    ctx.font = "14px Helvetica";
+    ctx.fillText("Loading "+pendingLoads+" of "+assetNames.length+" ...", 200, height/2);
+}
 
-// function checkLoadComplete() {
-//     // console.log("pending:", pendingLoads);
-//     if (pendingLoads == 0) {
-//         setupEvents();
-//         canvas.focus();
-//         ctx.drawImage(assets.background, 0, 0);
-//         globalState.clean = ctx.getImageData(0, 0, width, height);
-//         ctx.fillStyle = "yellow";
-//         ctx.font = "48px Helvetica";
-//         ctx.fillText("Press ENTER ...", 200, height/2);
-//     } else {
-//         showLoadProgress();
-//     }
-// }
+function checkLoadComplete() {
+    // console.log("pending:", pendingLoads);
+    if (pendingLoads == 0) {
+        setupEvents();
+        canvas.focus();
+        ctx.drawImage(assets.background, 0, 0);
+        globalState.clean = ctx.getImageData(0, 0, width, height);
+        globalState.level = "splash";
+    } else {
+        showLoadProgress();
+    }
+}
 
-// showLoadProgress();
-// assetNames.forEach(function(assetName) {
-//     var i = new Image();
-//     console.log("loading asset", assetName);
-//     i.onload = function() { pendingLoads--; checkLoadComplete(); }
-//     i.src = assetName + ".png";
-//     assets[assetName] = i;
-// }, this);
+showLoadProgress();
+assetNames.forEach(function(assetName) {
+    var i = new Image();
+    console.log("loading asset", assetName);
+    i.onload = function() { pendingLoads--; checkLoadComplete(); }
+    i.src = assetName + ".png";
+    assets[assetName] = i;
+}, this);
 
-// function setupEvents() {
-//     canvas.onkeydown = function(event) {
-//         console.log("keydown", event);
-//         //drawImage(assets.background, 0, 0);
-//         ctx.putImageData(globalState.clean, 0, 0);
-//     };
-// }
+function doKeydown(event) {
+    console.log("keydown", event);
+    // block other input while doing a move.
+    if (globalState.move) {
+        return;
+    }
+    if (globalState.level == "splash") {
+        if (event.keyCode == 13) {
+            setupLevel0();
+        }
+    }
+    if (globalState.level == "level0") {
+        switch (event.key) {
+            case 'a':
+                globalState.playerX -= globalState.movementSpeed;
+                if (globalState.playerX < 0) {
+                    globalState.playerX = width;
+                }
+                globalState.playerDirectionForward = false;
+                break;
+            case 'd':
+                globalState.playerX += globalState.movementSpeed;
+                if (globalState.playerX > width) {
+                    globalState.playerX = 0;
+                }
+                globalState.playerDirectionForward = true;
+                break;
+            case ' ':
+            case 'w':
+                globalState.moveTick = 0;
+                globalState.move = 'jump';
+                break;
+        }
+    }
+}
 
-var x = 150;
-var y = 150;
-// ctx.beginPath();
-// ctx.moveTo(0, 0);
-// ctx.arcTo(x, y, x-20, y+20, 20);
-// ctx.stroke();
+function setupEvents() {
+    canvas.onkeydown = doKeydown;
+}
 
-var gradientR = ctx.createLinearGradient(0,0,0,y);
-gradientR.addColorStop(0, "hsla(0, 100%, 50%, 0.0)");
-gradientR.addColorStop(1, 'hsla(0, 100%, 50%, 0.4)');
-
-var gradientY = ctx.createLinearGradient(0,0,0,y);
-gradientY.addColorStop(0, "hsla(0, 100%, 50%, 1)");
-gradientY.addColorStop(1, 'hsla(51, 100%, 50%, 0.5)');
-
+function setupLevel0() {
+    globalState.movementSpeed = 8;
+    globalState.level = "level0";
+    globalState.playerX = 10;
+    globalState.playerDirectionForward = true;
+}
 
 function draw(){
     globalTick++;
-    var w = 14;
-    if (globalTick % 16 == 0) {
-        ctx.globalCompositeOperation = 'destination-out';
-        ctx.fillStyle = 'hsla(0, 0%, 0%, .3)';
-        ctx.fillRect(0, 0, width, height);
-        ctx.globalCompositeOperation = 'source-over';
+    var dbg = "l:"+globalState.level + " t:"+globalTick + " m:"+globalState.move;
 
-        ctx.beginPath();
-        ctx.strokeStyle = "red";
-        ctx.arc(x, y, 22, 0, Math.PI, false);
-        ctx.lineTo(x + rand(-w, w), y-44);
-        ctx.lineTo(x+22, y);
-        ctx.fillStyle = gradientR;
-        ctx.fill();
-        ctx.stroke();
+    var playerOffsetY = 0;
 
-
-        ctx.beginPath();
-        ctx.strokeStyle = "yellow";
-        ctx.arc(x, y, 20, 0, Math.PI, false);
-        ctx.lineTo(x + rand(-w, w), y-40);
-        ctx.lineTo(x+20, y);
-        ctx.fillStyle = gradientY;
-        ctx.fill();
-        ctx.stroke();
+    if (globalState.move == 'jump') {
+        ++globalState.moveTick;
+        var moveDuration = 10;
+        var perTickMove = 4;
+        playerOffsetY = perTickMove * globalState.moveTick;
+        if (globalState.moveTick > moveDuration) {
+            // second half of anim is to go back down
+            playerOffsetY = perTickMove * (2*moveDuration - globalState.moveTick);
+        }
+        if (playerOffsetY == 0) {
+            // done!
+            globalState.move = null;
+        }
     }
+
+    switch (globalState.level) {
+        case "splash":
+            var alpha = (globalTick % 200)/100;
+            if (alpha > 1) { alpha = 2 - alpha; }
+            alpha = Math.exp(alpha-1);
+
+            ctx.putImageData(globalState.clean, 0, 0);
+
+            ctx.fillStyle = "rgba(255, 255, 0, "+alpha+")";
+            ctx.font = "48px Helvetica";
+            ctx.fillText("Press ENTER ...", 200, height/2);
+
+            dbg += " alpha:"+alpha;
+            break;
+        case "level0":
+            ctx.putImageData(globalState.clean, 0, 0);
+            ctx.save();
+            ctx.translate(globalState.playerX, 670 - playerOffsetY);
+            if (!globalState.playerDirectionForward) {
+                ctx.translate(assets.turtle.width, 0);
+                ctx.scale(-1, 1);
+            }
+            ctx.drawImage(assets.turtle, 0, 0);
+            ctx.restore();
+            dbg += " playerOffsetY: " + playerOffsetY;
+            break;
+    }
+
+    ctx.font = "14px Helvetica";
+    ctx.fillStyle = "red";
+    ctx.fillText(dbg, 10, 10);
+
     window.requestAnimationFrame(draw);
 }
 
